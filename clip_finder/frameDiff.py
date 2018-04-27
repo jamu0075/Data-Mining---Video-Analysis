2f239346d33a72710157d4c59905da3a399634f0
Jacob <jamu0075@colorado.edu>
2018-04-26 20:48:34 -0600

    Mined data points from videos

--- clip_finder/frameDiff.py
+++ clip_finder/frameDiff.py
@@ -2,38 +2,52 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+from argparse import ArgumentParser
 
 
-vidcap = cv2.VideoCapture('/home/user/DM_Videos/Indonesia_Raw.mp4') #30 fps
+parser = ArgumentParser(description="frame-differ: computes and stores frame differences")
+parser.add_argument('videofile', metavar='mp4file', help="the video to use")
+parser.add_argument('savefile', metavar='savefile', help="where to save data")
+args = parser.parse_args()
+
+vidcap = cv2.VideoCapture(args.videofile) #30 fps
 # fps = vidcap.get(cv2.cv.CV_CAP_PROP_FPS)
 # print "Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps)
-success,current_frame = vidcap.read()
-previous_frame = current_frame
+ok, previous_frame = vidcap.read()
+ok, current_frame = vidcap.read() if ok else (False, None)
 
-frame_diffs = []
-results = open("results-test.txt", "w+")
+counter = 0
+# frame_diffs = []
 
-#while(vidcap.isOpened()):
-for i in range(3600):
-    if i >= 1800:
+with open(args.savefile, "w+") as results:
+
+    while ok:
+
+        if counter % 1800 == 0:
+            print(counter)
+
+        counter += 1
+
         current_frame_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
         previous_frame_gray = cv2.cvtColor(previous_frame, cv2.COLOR_BGR2GRAY)
 
         frame_diff = cv2.absdiff(current_frame_gray,previous_frame_gray)
         results.write("%d\r\n" % (sum(sum(frame_diff))))
-        frame_diffs.append(sum(sum(frame_diff)))
+        #frame_diffs.append(sum(sum(frame_diff)))
         #cv2.imshow('frame diff ',frame_diff)
+        #
         # if cv2.waitKey(1) & 0xFF == ord('q'):
         #     break
         # if i%1000 == 0:
         #     print(i)
-        previous_frame = current_frame.copy()
-        ret, current_frame = vidcap.read()
+        previous_frame = current_frame
+        ok, current_frame = vidcap.read()
 
-results.close()
-#print(np.mean(frame_diffs))
+# print(np.mean(frame_diffs))
 #plt.plot(frame_diffs)
 #plt.show()
 
 vidcap.release()
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
+
+print("Done.")
